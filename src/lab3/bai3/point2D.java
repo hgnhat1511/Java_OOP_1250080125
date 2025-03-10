4f249c0c2137254ff593ea1d8709263b31818cbb
Hoang Nhat <riphacap@gmail.com>
2025-03-10 17:01:52 +0700

    update

--- src/lab3/bai3/point2D.java
+++ src/lab3/bai3/point2D.java
@@ -30,23 +30,3 @@ public class point2D {
         System.out.println("(" + x + ", " + y + ")");
     }
 }
-class Main {
-    public static void main(String[] args) {
-
-        point2D p1 = new point2D(3.5f, 4.5f);
-        point2D p2 = new point2D(1.5f, -2.5f);
-
-        System.out.print("Toa do p1: ");
-        p1.intoado();
-        System.out.print("Toa do p2: ");
-        p2.intoado();
-
-        point2D sum = p1.add(p2);
-        System.out.print("Tong hai diem: ");
-        sum.intoado();
-
-        point2D dxP1 = p1.dxtruchoanh();
-        System.out.print("Diem doi xung cua p1 qua truc hoanh: ");
-        dxP1.intoado();
-    }
-}
